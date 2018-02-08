b5679c3a7815d0aeb7c1632b5e6dde29cb275947
Dilshan Ganepola <ganepola.dilshan@gmail.com>
2018-02-08 20:59:46 +0530

    -V2 Table Design

---
+++ views/partials/header.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Patient Registration</title>
+    <link rel="stylesheet" href="../../WebDevProject/stylesheets/styleView.css">
+    <link rel="stylesheet" href="../../WebDevProject/stylesheets/style.css">
+    <link rel="stylesheet" href="../../WebDevProject/stylesheets/jquery.dataTables.css">
+</head>
+
+
